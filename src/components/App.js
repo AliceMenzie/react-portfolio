bc6ea211751ea0b522e0f9cc170dacf13dc3302f
Alice Menzie <Alice.Menzie@gmail.com>
2021-07-17 17:37:35 +1000

    project skeleton

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import NavBar from './NavBar';
+import Hero from './Hero';
+import Projects from './Projects';
+import TechStack from './TechStack';
+import Contact from './Contact';
+
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Hero />
+      <Projects />
+      <TechStack />
+      <Contact />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function NavBar() {
+    return (
+        <nav>
+            <div>NavBar</div>
+            <ul>
+                <li>Projects</li>
+                <li>Tech Stack</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+}
